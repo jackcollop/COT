79dd95a9e949452b4b04a78d99c1c32e1f3cc6f6
jackcollop <jackcollop@gmail.com>
2025-06-30 10:49:47 -0500

    Update cftc_lit.py

--- cftc_lit.py
+++ cftc_lit.py
@@ -12,11 +12,11 @@ fut['Date'] = pd.to_datetime(fut.report_date_as_yyyy_mm_dd)
 fut.sort_values(by=['Date'], inplace=True)
 fut.set_index('Date', inplace=True)
 
-fut['merc_net'] = fut['prod_merc_positions_long'] - fut['prod_merc_positions_short']
-fut['swap_net'] = fut['swap_positions_long_all'] - fut['swap__positions_short_all']
-fut['other_net'] = fut['other_rept_positions_long'] - fut['other_rept_positions_short']
-fut['money_net'] = fut['m_money_positions_long_all'] - fut['m_money_positions_short_all']
-fut['non_net'] = fut['nonrept_positions_long_all'] - fut['nonrept_positions_short_all']
+fut['merchant'] = fut['prod_merc_positions_long'] - fut['prod_merc_positions_short']
+fut['swap_dealer'] = fut['swap_positions_long_all'] - fut['swap__positions_short_all']
+fut['other_rept'] = fut['other_rept_positions_long'] - fut['other_rept_positions_short']
+fut['money'] = fut['m_money_positions_long_all'] - fut['m_money_positions_short_all']
+fut['non_rept'] = fut['nonrept_positions_long_all'] - fut['nonrept_positions_short_all']
 
 fut['money_net_old'] = fut['m_money_positions_long_old'] - fut['m_money_positions_short_old']
 fut['money_net_new'] = fut['m_money_positions_long_other'] - fut['m_money_positions_short_other']
@@ -30,13 +30,13 @@ index.set_index('Date', inplace=True)
 
 index['cit_net'] = index.cit_positions_long_all - index.cit_positions_short_all
 
-st.dataframe(fut[['merc_net','swap_net','other_net','money_net','non_net']].join(index[['cit_net','open_interest_all']]).diff().sort_index(ascending=False), column_config={'Date':st.column_config.DateColumn('Date')})
+st.dataframe(fut[['money','merchant','swap_dealer','other_rept','non_rept']].join(index[['cit_net','open_interest_all']]).diff().sort_index(ascending=False), column_config={'Date':st.column_config.DateColumn('Date')})
 
 fut['week'] = fut['yyyy_report_week_ww'].str.split(' ', expand=True)[3]
 fut['year'] = fut.index.year
 
 fut['money_net'] = fut['m_money_positions_long_all'] - fut['m_money_positions_short_all']
-fig = px.line(fut.reset_index()[['week','year','money_net']].pivot(index='week', columns='year', values='money_net'))
+fig = px.line(fut.reset_index()[['week','year','money']].pivot(index='week', columns='year', values='money_net'))
 
 fig.update_layout(
     xaxis = dict(
